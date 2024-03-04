0a67df91706b103b2b145165fdf7548d323c3263
Roy-Lee <ljunghwan928@naver.com>
2024-03-04 09:31:55 +0900

    update

---
+++ day07/Project48_obj/Project48_obj/clang48_objPointer.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+// 기본 클래스 Person 정의
+class Person
+{
+public:
+    // Sleep 함수: 모든 사람이 할 수 있는 행위
+    void Sleep() { cout << "Sleep" << endl; }
+};
+
+// Person 클래스를 상속받는 Student 클래스 정의
+class Student : public Person
+{
+public:
+    // Study 함수: 학생이 할 수 있는 특별한 행위
+    void Study() { cout << "Study" << endl; }
+};
+
+// Student 클래스를 상속받는 PartTimeStudent 클래스 정의
+class PartTimeStudent : public Student
+{
+public:
+    // Work 함수: 파트타임 학생이 할 수 있는 추가적인 행위
+    void Work() { cout << "Work" << endl; }
+};
+
+int main(void)
+{
+    // Person 타입의 포인터로 Student 객체를 동적 할당
+    Person* ptr1 = new Student();
+    // Person 타입의 포인터로 PartTimeStudent 객체를 동적 할당
+    Person* ptr2 = new PartTimeStudent();
+    // Student 타입의 포인터로 PartTimeStudent 객체를 동적 할당
+    Student* ptr3 = new PartTimeStudent();
+
+    // Person 클래스의 Sleep 함수 호출 (Student, PartTimeStudent 모두 접근 가능)
+    ptr1->Sleep(); // 출력: Sleep
+    ptr2->Sleep(); // 출력: Sleep
+
+    // Student 클래스의 Study 함수 호출 (PartTimeStudent는 Student를 상속받으므로 접근 가능)
+    ptr3->Study(); // 출력: Study
+
+    // 동적 할당된 메모리 해제
+    delete ptr1;
+    delete ptr2;
+    delete ptr3;
+
+    return 0;
+}
+
+/* 추가 설명
+1. Person 클래스는 모든 사람이 공통적으로 할 수 있는 Sleep 행위를 정의.
+
+2. Person 클래스를 상속받은 Student 클래스는 학생이 추가적으로 할 수 있는 Study 행위를 정의.
+
+3. Student 클래스를 다시 상속받은 PartTimeStudent 클래스는 파트타임 학생이 추가적으로 할 수 있는 Work 행위를 정의
+*/
